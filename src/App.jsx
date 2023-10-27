433d1ffc8af297311faac73ebe56ecb691f00698
Wilfren <wilfrenmoney@gmail.com>
2023-10-26 20:26:28 -0500

    ent2-gen31

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+        <h2>Weather App</h2>
+    </div>
+  )
+}
+
+export default App
