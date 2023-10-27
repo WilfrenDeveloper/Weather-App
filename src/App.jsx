012126d7e432e8923a799364866a314ef8780793
Wilfren <wilfrenmoney@gmail.com>
2023-10-26 21:36:37 -0500

    advances

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,44 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import CardWeather from './components/CardWeather'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  
+  const [coords, setCoords ] = useState()
+  const [weather, setWeather] = useState()
+
+  
+
+  const success = position => {
+    const obj = {
+      lat: position.coords.latitude,
+      lon: position.coords.longitude
+    }
+    setCoords(obj)
+  }
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(success)
+  }, [])
+  
+  console.log(coords);
+
+  useEffect(() => {
+    if(coords){
+      const APIKey = 'd253c4ce42914136aea7b48043b6d308'
+      const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKey}`
+      axios.get(url)
+    }
+  }, [])
+  
 
   return (
     <div>
         <h2>Weather App</h2>
+        <CardWeather 
+          weather={weather}
+        />
     </div>
   )
 }
---
+++ src/components/CardWeather.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const CardWeather = ({ weather }) => {
+
+
+
+  return (
+    <div>CardWeather</div>
+  )
+}
+
+export default CardWeather
