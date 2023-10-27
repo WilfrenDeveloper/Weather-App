378a24c882c09cf69914153de40f4f4344ddb8ef
Wilfren <wilfrenmoney@gmail.com>
2023-10-26 23:21:47 -0500

    advances

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,12 @@ import CardWeather from './components/CardWeather'
 
 function App() {
 
-  
-  const [coords, setCoords ] = useState()
-  const [weather, setWeather] = useState()
 
-  
+  const [coords, setCoords] = useState()
+  const [weather, setWeather] = useState()
+  const [temp, setTemp] = useState()
+  const [isLoading, setIsLoading] = useState(true)
+
 
   const success = position => {
     const obj = {
@@ -19,26 +20,40 @@ function App() {
   }
 
   useEffect(() => {
+    setIsLoading(true)
     navigator.geolocation.getCurrentPosition(success)
   }, [])
-  
+
   console.log(coords);
 
   useEffect(() => {
-    if(coords){
+    if (coords) {
       const APIKey = 'd253c4ce42914136aea7b48043b6d308'
-      const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKey}`
+      const url = `https://api.openweathermap.org/data/2.5/weather?lat=${coords.lat}&lon=${coords.lon}&appid=${APIKey}`
       axios.get(url)
+        .then(res => {
+          const celsius = (res.data.main.temp - 273.15).toFixed(1)
+          const fahrenheit = ((celsius * 9 / 5) + 32).toFixed(1)
+          setTemp({ celsius, fahrenheit })
+          setWeather(res.data)
+        }
+        )
+        .catch(error => console.log(error))
+        .finally(() => setIsLoading(false))
     }
-  }, [])
-  
+  }, [coords])
+
 
   return (
-    <div>
-        <h2>Weather App</h2>
-        <CardWeather 
-          weather={weather}
-        />
+    <div className='div'>
+      {
+        isLoading
+          ? <h2 className='div__h2'>loading...</h2>
+          : (<CardWeather
+            weather={weather}
+            temp={temp}
+          />)
+      }
     </div>
   )
 }
--- src/components/CardWeather.jsx
+++ src/components/CardWeather.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-const CardWeather = ({ weather }) => {
+const CardWeather = ({ weather, temp }) => {
 
+    const [isCelsius, setIsCelsius] = useState(true)
 
+    const handleChangeTemp= () => setIsCelsius(!isCelsius)
+    
 
   return (
-    <div>CardWeather</div>
+    <article className='article'>
+        <h1 className='article__h1'>Weather App</h1>
+        <h2 className='article__h2 h2'>{weather?.name}, {weather?.sys.country}</h2>
+        <section className='article__section'>
+            <header className='article__header'>
+                <img src="" alt="" className='article__img'/>
+            </header>
+            <article className='article__article'>
+                <h3 className='article
+                __h3'>"{weather?.weather[0].description}"</h3>
+                <ul>
+                    <li><span>Wind Speed</span> <span>{weather?.wind.speed}m/s</span></li>
+                    <li><span>Clouds</span> <span>{weather?.clouds.all}%</span></li>
+                    <li><span>Pressure</span> <span>{weather?.main.pressure}hPa</span></li>
+                </ul>
+            </article>
+        </section>
+        <h2>{isCelsius ? `${temp?.celsius} °C` : `${temp?.fahrenheit} °F`}</h2>
+        <button onClick={handleChangeTemp}>Change to {isCelsius ? '°F' : '°C'}</button>
+    </article>
+
   )
 }
 
